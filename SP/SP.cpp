bd15520cf10291e1ef723ef787e405418ea9a5fb
류일웅 <dlfdnd96@gmail.com>
2018-12-17 16:05:08 +0900

    쓰레드 추가

--- SP/SP.cpp
+++ SP/SP.cpp
@@ -1,4 +1,4 @@
-﻿// SP.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// server1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include "pch.h"
@@ -13,78 +13,28 @@
 #define SERVERPORT 9000
 #define BUFSIZE 1020000
 
-// 소켓 함수 오류 출력 후 종료
 void err_quit(const char *);
-
-// 소켓 함수 오류 출력
 void err_display(const char *);
-
-// 사용자 정의 데이터 수신 함수
 int recvn(SOCKET, char *, int, int);
-
-// 파일 기본 정보
 typedef struct Files
 {
 	char name[255];
 	unsigned int byte;
 }Files;
 
-void getISOTime(char* buffer, size_t bufferSize);
+DWORD WINAPI ProcessClient(LPVOID arg) {
 
-int main(int argc, char *argv[])
-{
-	// send, recv, 함수 츨력값 저장용
+	SOCKET client_sock = (SOCKET)arg;
 	int retval;
-
-	// (파일 크기 / 버퍼 사이즈) 계산한 값을 while문으로 돌리기 위한 변수
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
 	unsigned int count;
 
-	// 윈속 초기화
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return 1;
+	addrlen = sizeof(clientaddr);
 
-	// socket()
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
-
-	// bind()
-	SOCKADDR_IN serveraddr;
-	ZeroMemory(&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
-	if (retval == SOCKET_ERROR) err_quit("bind()");
-
-	// listen()
-	retval = listen(listen_sock, SOMAXCONN);
-	if (retval == SOCKET_ERROR) err_quit("listen()");
-
-	// 서버 대기 상태 완료----------------------------------
-
-	// 데이터 통신에 사용할 변수
-	SOCKET client_sock; // 클라이언트 저장 소켓
-	SOCKADDR_IN clientaddr; // 클라이언트 주소 저장
-	int addrlen; // 주소 길이
-	char buf[BUFSIZE]; // 전송하는 데이터
-
-	while (1)
-	{
-		// accept()
-		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
-		if (client_sock == INVALID_SOCKET)
-		{
-			err_display("accept()");
-			break;
-		}
-
-		// 접속한 클라이언트 정보 출력
-		printf("\n[TCP 서버] 클라이언트 접속: IP 주소 = %s, 포트 번호 = %d \n",
-			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
-
-		// 클라이언트로부터 파일 기본 정보 받기
+	getpeername(client_sock, (SOCKADDR*)&clientaddr, &addrlen);
+	while (1) {
 		FILE *fp = NULL;
 		Files files;
 
@@ -95,9 +45,7 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 
-		// 기존 파일 여부 확인
 		fp = fopen(files.name, "rb");
-
 		if (fp == NULL)
 			printf("같은 파일 이름이 없으므로 전송을 진행합니다.\n");
 		else
@@ -109,11 +57,6 @@ int main(int argc, char *argv[])
 			break;
 		}
 
-		char timeBuf[80];
-
-		getISOTime(timeBuf, sizeof(timeBuf));
-		printf("%s\n", timeBuf);
-
 		printf("파일을 전송 받습니다.\n");
 		printf("전송하는 파일: %s, 전송하는 파일 크기: %d Byte\n", files.name, files.byte);
 		printf("\n클라이언트로 부터 파일을 전송 받는 중 입니다.\n");
@@ -124,7 +67,6 @@ int main(int argc, char *argv[])
 
 		while (count)
 		{
-			// 받기
 			retval = recvn(client_sock, buf, BUFSIZE, 0);
 			if (retval == SOCKET_ERROR)
 			{
@@ -132,13 +74,11 @@ int main(int argc, char *argv[])
 				exit(1);
 			}
 
-			// 파일 작성 작업
 			fwrite(buf, 1, BUFSIZE, fp);
 
 			count--;
 		}
 
-		// 남은 파일 크기만큼 나머지 받기
 		count = files.byte - ((files.byte / BUFSIZE) * BUFSIZE);
 
 		retval = recvn(client_sock, buf, BUFSIZE, 0);
@@ -150,19 +90,70 @@ int main(int argc, char *argv[])
 
 		fwrite(buf, 1, count, fp);
 
-		// 파일 포인터 닫기
 		fclose(fp);
 
 		printf("\n파일 전송이 완료 되었습니다.\n");
 
-		getISOTime(timeBuf, sizeof(timeBuf));
-		printf("%s\n", timeBuf);
+		closesocket(client_sock);
+
+		return 0;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int retval;
+	unsigned int count;
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE];
+	HANDLE hThread;
+
+	while (1)
+	{
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET)
+		{
+			err_display("accept()");
+			break;
+		}
+
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소 = %s, 포트 번호 = %d \n",
+			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+		hThread = CreateThread(NULL, 0, ProcessClient, (LPVOID)client_sock, 0, NULL);
+		if (hThread == NULL) {
+			closesocket(client_sock);
+		}
+		else {
+			CloseHandle(hThread);
+		}
 	}
 
-	// 소켓 닫기
 	closesocket(listen_sock);
 
-	// 윈속 종료
 	WSACleanup();
 }
 
@@ -212,21 +203,6 @@ int recvn(SOCKET s, char *buf, int len, int flags)
 
 	return (len - left);
 }
-
-void getISOTime(char* buffer, size_t bufferSize) {
-	struct tm t;
-	time_t timer;
-
-	timer = time(NULL);    // 현재 시각을 초 단위로 얻기
-	localtime_s(&t, &timer); // 초 단위의 시간을 분리하여 구조체에 넣기
-
-
-	sprintf_s(buffer, bufferSize, "%04d-%02d-%02d %02d:%02d:%02d",
-		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
-		t.tm_hour, t.tm_min, t.tm_sec
-	);
-}
-
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
 // 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
--- SPClient/SPClient.cpp
+++ SPClient/SPClient.cpp
@@ -1,4 +1,4 @@
-﻿// SPClient.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// client2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include "pch.h"
@@ -8,21 +8,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #define SERVERIP "127.0.0.1"
 #define SERVERPORT 9000
 #define BUFSIZE 1020000
 
-// 소켓 함수 오류 출력 후 종료
 void err_quit(const char *);
-
-// 소켓 함수 오류 출력
 void err_display(const char *);
-
-// 사용자 정의 데이터 수신 함수
 int recvn(SOCKET, char *, int, int);
-
-// 파일 기본 정보
 typedef struct Files
 {
 	char name[255];
@@ -31,12 +25,14 @@ typedef struct Files
 
 int main(int argc, char *argv[])
 {
-	// (파일크기 / 버퍼 사이즈) 계산한 값을 while문으로 돌리기 위한 변수
 	unsigned int count;
 
-	// 파일 이름 및 크기 확인
 	FILE *fp;
 	Files files;
+	int i, j;
+	int sum = 0;
+	time_t start, end;
+	double result;
 
 	do
 	{
@@ -51,30 +47,21 @@ int main(int argc, char *argv[])
 		printf("FILE Pointer ERROR");
 		exit(1);
 	}
-	
-	// 파일 끝으로 위치 옮김
+
 	fseek(fp, 0L, SEEK_END);
-
-	// 파일 바이트값 출력
 	files.byte = ftell(fp);
-
-	// 다시 파일 처음으로 위치 옮김
 	fseek(fp, 0L, SEEK_SET);
 
-	// send, recv 함수 출력값 저장용
 	int retval;
 
-	// 윈속 초기화
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
 
-	// socket()
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 
-	// connect()
-	SOCKADDR_IN serveraddr; // 서버와 통신용 소켓
+	SOCKADDR_IN serveraddr;
 	ZeroMemory(&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
@@ -82,10 +69,8 @@ int main(int argc, char *argv[])
 	retval = connect(sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR) err_quit("connect()");
 
-	// 데이터 통신에 사용할 변수
-	char buf[BUFSIZE];	// 보낼 데이터를 저장할 공간
+	char buf[BUFSIZE];
 
-	// 파일 기본 정보 전송
 	retval = send(sock, (char *)&files, sizeof(files), 0);
 	if (retval == SOCKET_ERROR)
 	{
@@ -100,10 +85,9 @@ int main(int argc, char *argv[])
 		system("cls");
 		printf("전송하는 파일: %s, 전송하는 파일 크기: %d Byte\n", files.name, files.byte);
 
-		// 파일 읽어서 버퍼에 저장
+
 		fread(buf, 1, BUFSIZE, fp);
 
-		// 전송
 		retval = send(sock, buf, BUFSIZE, 0);
 		if (retval == SOCKET_ERROR)
 		{
@@ -111,12 +95,13 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 
-		printf("\n진행도: %d % %", (per - count) * 100 / per);
+		printf("\n진행도: %d % %", (per - count) * 100 / per, per);
+
 
 		count--;
+
 	}
 
-	// 남은 파일 크기만큼 나머지 전송
 	count = files.byte - (per * BUFSIZE);
 	fread(buf, 1, count, fp);
 
@@ -131,6 +116,7 @@ int main(int argc, char *argv[])
 	printf("전송하는 파일 : %s, 전송하는 파일 크기 : %d Byte\n", files.name, files.byte);
 	printf("\n진행도 : 100 %%");
 	printf("\n\n전송이 완료되었습니다.");
+
 
 	// closesocket()
 	closesocket(sock);
